f1713f4eef01c6dc6913f2f394fa5d0a7f20cfbd
Henry Stanley <henry@henrystanley.com>
2014-11-07 17:14:21 +0000

    implemented parser.read and parser.formatting_valid? methods

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,6 +2,15 @@ class Parser
 
   def read(file)
     contents = File.open(file, 'r') { |f| f.read }
+    raise unless formatting_valid?(contents)
+  end
+
+  def formatting_valid?(file)
+    if file.match(/[a-zA-Z]/)
+      false
+    else
+      true
+    end
   end
 
 end
