093b61d38ab7c5c927c7efaf7ea62df2bd142b92
Henry Stanley <henry@henrystanley.com>
2014-11-13 11:36:40 +0000

    Theatre contains rows

--- lib/theatre.rb
+++ lib/theatre.rb
@@ -1,5 +1,12 @@
 class Theatre
 
+  attr_reader :rows
 
+  NUMBER_OF_ROWS = 100
+
+  def initialize
+    @rows = []
+    NUMBER_OF_ROWS.times { @rows << Row.new }
+  end
 
 end
--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -4,7 +4,17 @@ describe 'Theatre' do
 
   let(:theatre) { Theatre.new }
 
-  xit 'contains rows of seats'
+  it 'contains rows' do
+    expect(theatre.rows).to be_an(Array)
+    theatre.rows.each do |row|
+      expect(row.class).to eq(Row)
+    end
+  end
+
+  it 'contains the correct number of rows' do
+    expect(theatre.rows.count).to eq(Theatre::NUMBER_OF_ROWS)
+  end
+
 
 
 
